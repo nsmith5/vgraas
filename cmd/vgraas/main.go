15cbff329a4a5d8d756785d85ebd9a823932a85d
Nathan Smith <nathan@felixandpaul.com>
2019-05-25 12:19:50 -0700

    Change hello-world code to be httpserver
    This helps make CI/CD that will reflect our
    end goals better than simply printing something
    out.

--- cmd/vgraas/main.go
+++ cmd/vgraas/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-	"os"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/nsmith5/vgraas/pkg/vgraas"
 )
 
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	err := vgraas.Hello(w)
+	if err != nil {
+		w.WriteHeader(500)
+		io.WriteString(w, "Failed to write hello :(")
+		return
+	}
+	return
+}
+
 func main() {
-	vgraas.Hello(os.Stdout)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HelloHTTP)
+	log.Println(http.ListenAndServe(":8080", mux))
 }
